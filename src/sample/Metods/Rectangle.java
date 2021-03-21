210b089d962b9270bbe59b255babc78d884a346f
CarryOblivion <80450495+CarryObilvion@users.noreply.github.com>
2021-03-21 17:24:56 +0300

    Add files via upload

---
+++ src/sample/Metods/Rectangle.java
@@ -0,0 +1,13 @@
+package sample.Metods;
+
+import javafx.scene.layout.Pane;
+
+/**
+ * Интерфейс и его реализация
+ * @author Kozhevnikov Alexander pPi-171
+ * @version 1.2
+ */
+
+public interface Rectangle {
+    void draw(Pane pane);
+}
